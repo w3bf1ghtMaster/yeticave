3624269c9a1c23ce3b08995cf41629b07863216e
w3bf1ghtFeature <w3bf1ght@mail.ru>
2019-08-19 17:42:16 +0300

    iterating over arrays

--- index.php
+++ index.php
@@ -2,6 +2,42 @@
 $is_auth = rand(0, 1);
 
 $user_name = 'Darth Vader'; // укажите здесь ваше имя
+
+$cats = ['Доски и лыжи','Крепления','Ботинки','Одежда','Инструмены','Разное'];
+
+$cards = [
+    [
+        'title'=> '2014 Rossignol District Snowboard',
+        'category' => 'Доски и лыжи',
+        'price' => '10999',
+        'url' => '/img/lot-1.jpg'],
+    [
+        'title'=> 'DC Ply Mens 2016/2017 Snowboard',
+        'category' => 'Доски и лыжи',
+        'price' => '159999',
+        'url' => '/img/lot-2.jpg'],
+    [
+        'title'=> 'Крепления Union Contact Pro 2015 года размер L/XL',
+        'category' => 'Крепления',
+        'price' => '8000',
+        'url' => '/img/lot-3.jpg'],
+    [
+        'title'=> 'Ботинки для сноуборда DC Mutiny Charocal',
+        'category' => 'Ботинки',
+        'price' => '10999',
+        'url' => '/img/lot-4.jpg'],
+    [
+        'title'=> 'Куртка для сноуборда DC Mutiny Charocal',
+        'category' => 'Одежда',
+        'price' => '7500',
+        'url' => '/img/lot-5.jpg'],
+    [
+        'title'=> 'Маска Oakley Canopy',
+        'category' => 'Разное',
+        'price' => '5400',
+        'url' => '/img/lot-6.jpg']
+         ];
+
 ?>
 <!DOCTYPE html>
 <html lang="ru">
@@ -57,9 +93,11 @@ $user_name = 'Darth Vader'; // укажите здесь ваше имя
         <p class="promo__text">На нашем интернет-аукционе ты найдёшь самое эксклюзивное сноубордическое и горнолыжное снаряжение.</p>
         <ul class="promo__list">
             <!--заполните этот список из массива категорий-->
+            <?php foreach ($cats as $cat) :?>
             <li class="promo__item promo__item--boards">
-                <a class="promo__link" href="pages/all-lots.html">Имя категории</a>
+                <a class="promo__link" href="pages/all-lots.html"><?=$cat?></a>
             </li>
+            <?php endforeach;?>
         </ul>
     </section>
     <section class="lots">
@@ -68,17 +106,18 @@ $user_name = 'Darth Vader'; // укажите здесь ваше имя
         </div>
         <ul class="lots__list">
             <!--заполните этот список из массива с товарами-->
+            <?php foreach ($cards as $card) :?>
             <li class="lots__item lot">
                 <div class="lot__image">
-                    <img src="" width="350" height="260" alt="">
+                    <img src="<?=$card['url']?>" width="350" height="260" alt="<?=$card['title']?>">
                 </div>
                 <div class="lot__info">
-                    <span class="lot__category">Название категории</span>
-                    <h3 class="lot__title"><a class="text-link" href="pages/lot.html">Название товара</a></h3>
+                    <span class="lot__category"><?=$card['category']?></span>
+                    <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?=$card['title']?></a></h3>
                     <div class="lot__state">
                         <div class="lot__rate">
                             <span class="lot__amount">Стартовая цена</span>
-                            <span class="lot__cost">цена<b class="rub">р</b></span>
+                            <span class="lot__cost"><?=($card['price']);?><b class="rub">р</b></span>
                         </div>
                         <div class="lot__timer timer">
                             12:23
@@ -86,6 +125,7 @@ $user_name = 'Darth Vader'; // укажите здесь ваше имя
                     </div>
                 </div>
             </li>
+            <?php endforeach;?>
         </ul>
     </section>
 </main>
@@ -95,9 +135,11 @@ $user_name = 'Darth Vader'; // укажите здесь ваше имя
     <nav class="nav">
         <ul class="nav__list container">
             <!--заполните этот список из массива категорий-->
+            <?php foreach ($cats as $cat) :?>
             <li class="nav__item">
-                <a href="pages/all-lots.html">Название категории</a>
+                <a href="pages/all-lots.html"><?=$cat?></a>
             </li>
+            <?php endforeach;?>
         </ul>
     </nav>
     <div class="main-footer__bottom container">
